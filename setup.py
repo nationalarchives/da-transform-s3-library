cb1f3d266b17348d672e23d5903575bd16501e9e
Alex Ford <alex.ford@nationalarchives.gov.uk>
2022-12-13 16:02:31 +0000

    Harmonise workflow files, add tag from environment.

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 import setuptools
+import os
+
+build_version = os.environ['BUILD_VERSION']
 
 setuptools.setup(
     name='s3-lib',
-    version="0.0.8",
+    version=build_version,
     description='APIs for managing AWS s3 content',
     packages=['s3_lib'],
     python_requires='>=3.8'
